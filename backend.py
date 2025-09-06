46cdbaf5144827d8d76a6b69fb2f37c69cd265db
vansh-9878 <aroravansh0501@gmail.com>
2025-09-06 20:40:02 +0530

    added security

--- backend.py
+++ backend.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException,Request
 from main import check_email
+import os
 
 app=FastAPI()
+WEBHOOK_KEY = os.getenv("WEBHOOK_KEY")
 
 @app.get("/")
 def status():
@@ -10,7 +12,11 @@ def status():
     }
 
 @app.get("/check")
-def checkMails():
+def checkMails(request: Request, key: str = None):
+    if WEBHOOK_KEY:
+        header = request.headers.get("x-api-key")
+        if header != WEBHOOK_KEY and key != WEBHOOK_KEY:
+            raise HTTPException(status_code=401, detail="Unauthorized")
     try:
         check_email()
         return{
